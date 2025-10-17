4e448bf7d8e02b2f20d420d74ccce40ad175b16b
abijoserah <serahabijo94@gmail.com>
2025-10-17 21:51:27 +0200

    Display data in a components

--- src/components/PokemonCard.tsx
+++ src/components/PokemonCard.tsx
@@ -11,16 +11,16 @@ const pokemonList = [
 
 
 export function PokemonCard() {
-	const pokemon = pokemonList[0]
+	const pokemon = pokemonList[1]
 
-	return pokemon.imgSrc ? (
+	return (
 		<figure>
-			<img
+			{pokemon.imgSrc ? ( <img
 				src={pokemon.imgSrc}
 				alt={pokemon.name}
-			/>
+			/> ) : (<p>???</p>) }
 			<figcaption>{pokemon.name}</figcaption>
 		</figure>
-	) : <p>???</p> 
+	) 
 }
 
