b3a20aab8221fe81598b15043676d768bf8ee6a7
abijoserah <serahabijo94@gmail.com>
2025-10-18 17:55:54 +0200

    Working with component props

--- src/components/PokemonCard.tsx
+++ src/components/PokemonCard.tsx
@@ -1,26 +1,12 @@
-const pokemonList = [
-  {
-    name: "bulbasaur",
-    imgSrc:
-      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
-  },
-  {
-    name: "mew",
-  },
-];
-
-
-export function PokemonCard() {
-	const pokemon = pokemonList[1]
-
+export function PokemonCard({ pokemon }) {
 	return (
 		<figure>
-			{pokemon.imgSrc ? ( <img
-				src={pokemon.imgSrc}
-				alt={pokemon.name}
-			/> ) : (<p>???</p>) }
+			{pokemon.imgSrc ? (
+				<img src={pokemon.imgSrc} alt={pokemon.name} />
+			) : (
+				<p>???</p>
+			)}
 			<figcaption>{pokemon.name}</figcaption>
 		</figure>
-	) 
+	);
 }
-
